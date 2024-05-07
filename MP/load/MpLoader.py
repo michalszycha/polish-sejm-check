478919f827b24df0851404fb86f69b172d6c7dc6
michalszycha <michalsz378@o2.pl>
2024-05-07 23:10:13 +0200

    fix file names in .yml file

--- MP/load/MpLoader.py
+++ MP/load/MpLoader.py
@@ -3,17 +3,17 @@ from sqlalchemy import create_engine, inspect, text
 
 
 def load_mps_to_sql(mps: pd.DataFrame):
-    engine = create_engine("sqlite:///MPs.db")
+    engine = create_engine("sqlite:///mps.db")
     inspector = inspect(engine)
     connection = engine.connect()
 
-    if inspector.has_table("MPs"):
-        connection.execute(text("DROP TABLE MPs"))
+    if inspector.has_table("mps"):
+        connection.execute(text("DROP TABLE mps"))
 
-    mps.to_sql("MPs", engine, index=False)
+    mps.to_sql("mps", engine, index=False)
 
     connection.close()
 
 
 def load_mp_to_csv(mps: pd.DataFrame):
-    mps.to_csv("MPs.csv", sep=',')
+    mps.to_csv("mps.csv", sep=',')
