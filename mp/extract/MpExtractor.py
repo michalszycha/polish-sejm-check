e17fa2d0e63d6ec34318046670b34e1fa7959398
michalszycha <michalsz378@o2.pl>
2024-06-07 01:23:32 +0200

    change to pass term when running script

--- mp/extract/MpExtractor.py
+++ mp/extract/MpExtractor.py
@@ -1,8 +1,9 @@
 import requests
 import pandas as pd
+import config.config as config
 
 
 def get_mps() -> pd.DataFrame:
-    response = requests.get('https://api.sejm.gov.pl/sejm/term9/MP')
+    response = requests.get(f"https://api.sejm.gov.pl/sejm/term{config.term}/MP")
     data = response.json()
     return pd.DataFrame.from_dict(data)
