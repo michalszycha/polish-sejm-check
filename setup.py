191f44424614b668c6271c96ca0b7cbeadf5aa9c
michalszycha <michalsz378@o2.pl>
2025-05-29 23:21:41 +0200

    add setup to install modules locally

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='polish-sejm-check', # Choose a unique name for your project
+    version='0.1.0',
+    packages=find_packages(),
+    # Add any other dependencies your modules might have (e.g., pandas)
+    install_requires=[
+        'pandas',
+        'requests',
+        'asyncio',
+        'aiohttp',
+
+    ],
+    # You might need to include data files if your modules use them
+    include_package_data=True,
+    # package_data={
+    #     'your_project_name': ['data/*.csv'],
+    # },
+)
